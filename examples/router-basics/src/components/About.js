237fc391904b5a286cceea69f93ad969016d95c3
SAUMYAJIT <saumyajit987@gmail.com>
2025-06-28 21:04:46 +0530

    Documentation

---
+++ examples/router-basics/src/components/About.js
@@ -0,0 +1,10 @@
+function About(){
+    return(
+        <div>
+            <h1>About Page</h1>
+            <p>This is basic react router tutorial</p>
+        </div>
+    )
+}
+
+export default About ; 
---
+++ examples/router-basics/src/components/Home.js
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <p>This is 1st react web page</p>
+        </div>
+    )
+}
+
+export default Home ; 
